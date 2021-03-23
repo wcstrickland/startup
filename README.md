020bb394e3f13edb0d5cb89ff1c9ebb746458a78
Station1 <36therapy1@auroracares.local>
2021-03-23 07:40:57 -0500

    created git repo for configuration, vim, bash scripts and dotfiles
    Update install.sh
    
    Update README.txt
    
    Update README.txt
    
    Update install.sh
    
    Update .vimrc
    
    Update and rename .vimrc to vimrc
    
    Update plugless
    
    Rename vimrc to .vimrc
    
    Add files via upload
    
    Update config
    
    Update install.sh
    
    Delete javascript.vim
    
    Update README.txt
    
    Update .vimrc
    
    added leader snippets
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update plugless
    
    Update plugless
    
    Update plugless
    
    Update plugless
    
    Rename .vimrc to ide
    
    Rename plugless to .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Add files via upload
    
    Add files via upload
    
    Update install.sh
    
    Update config
    
    Update .bashrc
    
    Update config
    
    Update install.sh
    
    Update README.txt
    
    Update install.sh
    
    Update .vimrc
    
    Update .vimrc
    
    Create noSleep.txt
    
    Add files via upload
    
    Update install.sh
    
    Update config
    
    Add files via upload
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Update .vimrc
    
    Add files via upload
    
    Update install.sh
    
    updated js docs file
    
    Update .vimrc
    
    added folding
    
    added auto wallpaper to bash script
    
    added git cheatsheet
    
    added vim support for git cheatsheet
    
    Update .vimrc
    
    added spell check macro
    
    Removed boiler plate js file as it has been published as a stand alone
    NPM package
    
    Rename docs.js to docs.txt
    
    Update .vimrc
    
    Update install.sh
    
    Update config
    
    Add files via upload
    
    Update README.txt
    
    Rename README.txt to README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    added making a .vim directory to the install script
    
    added auto session saving

---
+++ README.md
@@ -0,0 +1,10 @@
+run `sudo bash install.sh` from inside the startup directory while it is housed in your home directory
+
+.vimrc and .Xresources go in ~/
+
+config and i3blocks.conf go in ~/.config/i3/
+
+
+https://code.visualstudio.com/download
+
+run `sudo dpkg -i ./vscodefilename.deb` to install vscode
---
+++ install.sh
@@ -0,0 +1,12 @@
+apt install vim feh nodejs vlc htop ranger terminator neofetch compton rofi
+mkdir ../.config/i3/
+mkdir ../.vim
+cp docs.txt ../.vim/
+cp git.txt ../.vim/
+cp .Xresources ../
+cp .vimrc ../
+cp .bashrc ../
+cp wall.jpg ../Pictures/
+cp config ../.config/i3/
+cp i3blocks.conf  ../.config/i3/
+feh --bg-scale ../Pictures/wall.jpg
